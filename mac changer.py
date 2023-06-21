53c446b80a9847d3d478c1919b8de0dcc7a86f41
judonnah <46363883+judonnah@users.noreply.github.com>
2023-06-21 03:57:30 +0100

    init

---
+++ mac changer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import subprocess
+subprocess.call("ifconfig wlan0 down", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 11:22:33:44:55:66 ", shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
